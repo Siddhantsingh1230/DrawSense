f3ca31280a39f171f31daffc00d42af41eb028f0
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2023-12-14 15:09:07 +0530

    updated project

--- scripts/DrawSense.js
+++ scripts/DrawSense.js
@@ -10,10 +10,11 @@ class DrawSense {
     this.isDrawing = false;
     //apending the canvas into the container
     container.appendChild(this.canvas);
-    this.#redraw();
+    this.#render();
   }
   #undo = document.getElementById("undo");
   #reset = document.getElementById("reset");
+  #recognize = document.getElementById("recognize");
   #eventListeners = (container) => {
     this.canvas.onmousedown = (evt) => {
       const mouse = this.#getMouse(evt);
@@ -28,13 +29,13 @@ class DrawSense {
         const mouse = this.#getMouse(evt);
         const lastPath = this.paths[this.paths.length - 1];
         lastPath.push(mouse);
-        this.#redraw(this.paths);
+        this.#render(this.paths);
       }
     };
     // touch events
     document.ontouchend = (evt) => {
       const loc = evt.touches[0];
-      this.canvas.onmouseup(loc);
+      document.onmouseup(loc);
     };
     this.canvas.ontouchstart = (evt) => {
       const loc = evt.touches[0];
@@ -47,16 +48,34 @@ class DrawSense {
     document.body.onresize = (evt) => {
       this.canvas.width = container.clientWidth;
       this.canvas.height = container.clientHeight;
-      console.log("resize");
     };
     // control buttons
     this.#undo.onclick = () => {
-      this.paths.pop();
-      this.#redraw();
+      if (this.paths.length) {
+        this.paths.pop();
+        this.#render();
+      }
     };
     this.#reset.onclick = () => {
-      this.paths = [];
-      this.#redraw();
+      if (this.paths.length) {
+        this.paths = [];
+        this.#render();
+      }
+    };
+    this.#recognize.onclick = () => {
+      if (this.paths.length) {
+        const figure = JSON.stringify(this.paths);
+        const downloadATag = document.createElement("a");
+        downloadATag.setAttribute(
+          "href",
+          "data:text/plain:charset=utf-8" + encodeURIComponent(figure)
+        );
+        const filename = new Date().getTime() + ".json";
+        downloadATag.setAttribute("download", filename);
+        document.body.appendChild(downloadATag);
+        downloadATag.click();
+        document.body.removeChild(downloadATag);
+      }
     };
   };
   #getMouse = (evt) => {
@@ -66,7 +85,7 @@ class DrawSense {
       Math.round(evt.clientY - canvasRect.top),
     ];
   };
-  #redraw = () => {
+  #render = () => {
     this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
     draw.paths(this.ctx, this.paths);
     if (this.paths.length > 0) {
@@ -74,11 +93,13 @@ class DrawSense {
       this.#reset.disabled = false;
       this.#reset.style.cursor = "auto";
       this.#undo.style.cursor = "auto";
+      this.#recognize.style.cursor = "auto";
     } else {
       this.#undo.disabled = true;
       this.#reset.disabled = true;
       this.#reset.style.cursor = "not-allowed";
       this.#undo.style.cursor = "not-allowed";
+      this.#recognize.style.cursor = "not-allowed";
     }
   };
 }
